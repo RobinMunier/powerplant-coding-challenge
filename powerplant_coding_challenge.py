284026ab25681e1e151238047eee2d6b7251ee0b
Robin Munier <60067686+RobinMunier@users.noreply.github.com>
2021-11-18 21:49:25 +0100

    Add files via upload

--- powerplant_coding_challenge.py
+++ powerplant_coding_challenge.py
@@ -31,26 +31,26 @@ from itertools import combinations
 
 def preprocessing(data):
 
-    """ Function to extract the useful information from the dictionary input file.
+  """ Function to extract the useful information from the dictionary input file.
 
-    Parameters
-    ----------
-    data : dict
-      Dict containing the data from the input json file.
-        
-    Returns
-    -------
-    powerplants : DataFrame
-      A DataFrame whose each row represents a powerplant and the columns gather the useful information about it (pmin, pmax, etc.).
-    groups : DataFrame
-      A DataFrame whose each row represents a group of "mergeable" powerplants and the columns gather the useful information about it (pmin, pmax, etc.).
-      Using these groups allows to decrease the computing time of the whole algorithm when the number of powerplants is large.
-    load : float or int
-      The load extracted from the data dictionary.        
+  Parameters
+  ----------
+  data : dict
+    Dict containing the data from the input json file.
+      
+  Returns
+  -------
+  powerplants : DataFrame
+    A DataFrame whose each row represents a powerplant and the columns gather the useful information about it (pmin, pmax, etc.).
+  groups : DataFrame
+    A DataFrame whose each row represents a group of "mergeable" powerplants and the columns gather the useful information about it (pmin, pmax, etc.).
+    Using these groups allows to decrease the computing time of the whole algorithm when the number of powerplants is large.
+  load : float or int
+    The load extracted from the data dictionary.        
     
-    """
+  """
 
-  def make_groups(df, cumulate=False):
+  def make_groups(df):
     df['p_range']=df['pmax']-df['pmin']
     groups=pd.DataFrame()
     for price in df['price'].unique():
@@ -105,21 +105,21 @@ def preprocessing(data):
 
 
 def get_strategy(groups, load):
-    """ Function to compute the best production strategy.
+  """ Function to compute the best production strategy.
 
-    Parameters
-    ----------
-    groups : DataFrame
-      A DataFrame whose each row represents a group of "mergeable" powerplants and the columns gather the useful information about it (pmin, pmax, etc.).
-    load : float or int
-      The load extracted from the data dictionary. 
+  Parameters
+  ----------
+  groups : DataFrame
+    A DataFrame whose each row represents a group of "mergeable" powerplants and the columns gather the useful information about it (pmin, pmax, etc.).
+  load : float or int
+    The load extracted from the data dictionary. 
         
-    Returns
-    -------
-    strategy : dict
-      Dictionary with two keys ("units" and "p") whose values are lists containing the groups to use for production and the power each one should deliver.
+  Returns
+  -------
+  strategy : dict
+    Dictionary with two keys ("units" and "p") whose values are lists containing the groups to use for production and the power each one should deliver.
     
-    """
+  """
   strategy = {'units':[], 'p':[]}
   best_price = np.inf
 
@@ -153,25 +153,25 @@ def get_strategy(groups, load):
 
 
 def share(x, load, powerplants):
-    """ Recursive function computing the best strategy to use in terms of units (the units to use for production and the power each one should deliver) 
-    from the best strategy defined in terms of groups (the groups to use for production and the power each one should deliver).
+  """ Recursive function computing the best strategy to use in terms of units (the units to use for production and the power each one should deliver) 
+  from the best strategy defined in terms of groups (the groups to use for production and the power each one should deliver).
 
-    Parameters
-    ----------
-    x : list
-      A list of lists containing the units name constituting a particular group in the order they have been "merged" to create this group.
-    load : float or int
-      The load we want to generate with units from the group related to the x argument. 
-    powerplants : DataFrame
-      A DataFrame whose each row represents a powerplant and the columns gather the useful information about it (pmin, pmax, etc.).
-        
-    Returns
-    -------
-    results : dict
-      Dictionary whose keys are the name of the units constituting the group and the associated values are the power these units should deliver in order to optimally
-      generate the input load. 
+  Parameters
+  ----------
+  x : list
+    A list of lists containing the units name constituting a particular group in the order they have been "merged" to create this group.
+  load : float or int
+    The load we want to generate with units from the group related to the x argument. 
+  powerplants : DataFrame
+    A DataFrame whose each row represents a powerplant and the columns gather the useful information about it (pmin, pmax, etc.).
       
-    """
+  Returns
+  -------
+  results : dict
+    Dictionary whose keys are the name of the units constituting the group and the associated values are the power these units should deliver in order to optimally
+    generate the input load. 
+      
+  """
   results={}
   if len(x)==1:
     results[x[0]]=load
@@ -199,26 +199,26 @@ def share(x, load, powerplants):
 
 
 def get_results(strategy, groups, powerplants, share=share):
-    """ Function to write the results of the optimization of the strategy in a dictionary, in the format required by the rules of the challenge.
+  """ Function to write the results of the optimization of the strategy in a dictionary, in the format required by the rules of the challenge.
  
-    Parameters
-    ----------
-    strategy : dict
-      Dictionary with two keys ("units" and "p") whose values are lists containing the groups to use for production and the power each one should deliver.
-    groups : DataFrame
-      A DataFrame whose each row represents a group of "mergeable" powerplants and the columns gather the useful information about it (pmin, pmax, etc.).
-    powerplants : DataFrame
-      A DataFrame whose each row represents a powerplant and the columns gather the useful information about it (pmin, pmax, etc.).
-    share : func
-      Function computing the best strategy to use in terms of units (the units to use for production and the power each one should deliver) 
-      from the best strategy defined in terms of groups (the groups to use for production and the power each one should deliver).
-        
-    Returns
-    -------
-    results : dict
-      Dictionary with two keys ("units" and "p") whose values are lists containing the units and the power each one should deliver.    
-    
-    """
+  Parameters
+  ----------
+  strategy : dict
+    Dictionary with two keys ("units" and "p") whose values are lists containing the groups to use for production and the power each one should deliver.
+  groups : DataFrame
+    A DataFrame whose each row represents a group of "mergeable" powerplants and the columns gather the useful information about it (pmin, pmax, etc.).
+  powerplants : DataFrame
+    A DataFrame whose each row represents a powerplant and the columns gather the useful information about it (pmin, pmax, etc.).
+  share : func
+    Function computing the best strategy to use in terms of units (the units to use for production and the power each one should deliver) 
+    from the best strategy defined in terms of groups (the groups to use for production and the power each one should deliver).
+       
+  Returns
+  -------
+  results : dict
+    Dictionary with two keys ("units" and "p") whose values are lists containing the units and the power each one should deliver.    
+  
+  """
   results = []
   for (p,unit) in zip(strategy['p'], strategy['units']):
     if unit==0: # handle wind case
@@ -239,24 +239,24 @@ def get_results(strategy, groups, powerplants, share=share):
 
 
 def plan(data):
-    """ Function to compute and write in a dictionary the optimal production plan associated to a dictionary input in the same format as the input files provided for 
-    the challenge.
+  """ Function to compute and write in a dictionary the optimal production plan associated to a dictionary input in the same format as the input files provided for 
+  the challenge.
     
-    Parameters
-    ----------
-    data : dict
-      Dict containing the data from the input json file.
-      
-    Returns
-    -------
-    results : dict
-      Dictionary with two keys ("units" and "p") whose values are lists containing the units and the power each one should deliver.    
+  Parameters
+  ----------
+  data : dict
+    Dict containing the data from the input json file.
     
-    """
-    powerplants, groups, load = preprocessing(data)
-    strategy = get_strategy(groups, load)
-    results = get_results(strategy, groups, powerplants, share)
-    return results
+  Returns
+  -------
+  results : dict
+    Dictionary with two keys ("units" and "p") whose values are lists containing the units and the power each one should deliver.    
+    
+  """
+  powerplants, groups, load = preprocessing(data)
+  strategy = get_strategy(groups, load)
+  results = get_results(strategy, groups, powerplants, share)
+  return results
 
 
 # Initialization of the API
